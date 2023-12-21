87f3381b5a614eadaf8131197a99d5d00be859f5
Agu Chux <agu.chux@yahoo.com>
2023-12-21 15:06:52 +0000

    App Done

--- src/app/components/forms/Testimonies.tsx
+++ src/app/components/forms/Testimonies.tsx
@@ -5,7 +5,8 @@ type Props = {}
 
 const Testimonies = async (props: Props) => {
     const [busy, setBusy] = React.useState(false);
-    const response = await fetch('/api/testimonials', {
+    const apiUri = process.env.NEXT_URI || 'http://localhost:3000';
+    const response = await fetch(`${apiUri}/api/testimonials`, {
         next: {
             revalidate: 10
         }
@@ -43,77 +44,6 @@ const Testimonies = async (props: Props) => {
                     </div>
                 </div>
             </div>
-
-            {/* <section className="testimonials-section section-ptb" data-aos="fade-up">
-                <div className="testimonials-area">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-12">
-                                <div className="section-title text-center mb-4">
-                                    <h4>Testimonials</h4>
-                                    <h2>Our Student Saying...</h2>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-md-12">
-                                <div className="testimonials-wrapper">
-                                    <div className="single-testimonial border-2 p-4">
-                                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla sint occaecat cupidatat non proident, sunt in culpa.</p>
-                                        <div className="testimonial-referance">
-                                            <p><strong>Jhon Smith; </strong> CEO &amp; Founder</p>
-                                        </div>
-                                    </div>
-                                    <div className="single-testimonial border-2 p-4">
-                                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla sint occaecat cupidatat non proident, sunt in culpa.</p>
-                                        <div className="testimonial-referance">
-                                            <p><strong>Jhon Smith; </strong> CEO &amp; Founder</p>
-                                        </div>
-                                    </div>
-                                    <div className="single-testimonial border-2 p-4">
-                                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla sint occaecat cupidatat non proident, sunt in culpa.</p>
-                                        <div className="testimonial-referance">
-                                            <p><strong>Jhon Smith; </strong> CEO &amp; Founder</p>
-                                        </div>
-                                    </div>
-                                    <div className="single-testimonial border-2 p-4">
-                                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla sint occaecat cupidatat non proident, sunt in culpa.</p>
-                                        <div className="testimonial-referance">
-                                            <p><strong>Jhon Smith; </strong> CEO &amp; Founder</p>
-                                        </div>
-                                    </div>
-                                    <div className="single-testimonial border-2 p-4">
-                                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla sint occaecat cupidatat non proident, sunt in culpa.</p>
-                                        <div className="testimonial-referance">
-                                            <p><strong>Jhon Smith; </strong> CEO &amp; Founder</p>
-                                        </div>
-                                    </div>
-                                    <div className="single-testimonial border-2 p-4">
-                                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla sint occaecat cupidatat non proident, sunt in culpa.</p>
-                                        <div className="testimonial-referance">
-                                            <p><strong>Jhon Smith; </strong> CEO &amp; Founder</p>
-                                        </div>
-                                    </div>
-                                    <div className="single-testimonial border-2 p-4">
-                                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla sint occaecat cupidatat non proident, sunt in culpa.</p>
-                                        <div className="testimonial-referance">
-                                            <p><strong>Jhon Smith; </strong> CEO &amp; Founder</p>
-                                        </div>
-                                    </div>
-                                    <div className="single-testimonial border-2 p-4">
-                                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla sint occaecat cupidatat non proident, sunt in culpa.</p>
-                                        <div className="testimonial-referance">
-                                            <p><strong>Jhon Smith; </strong> CEO &amp; Founder</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section> */}
-
-
         </>
     )
 }
