6a757d95025f6ee86391c03dacb89d697ac17326
Agu Chux <agu.chux@yahoo.com>
2023-12-22 22:23:34 +0000

    Settings up

--- src/components/AdminFooter.tsx
+++ src/components/AdminFooter.tsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import BaseFooter from './BaseFooter'
+import Link from 'next/link'
 
 const AdminFooter = () => {
     return (
         <>
             <footer>
                 <div className="footer-area default_bg">
-                    <BaseFooter />
+                    <div className="footer-bottom-wrapper border-top py-3">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="footer-bottom">
+                                        <div className="copyright-text">
+                                            <p>&copy;<strong>The Recruitment Consult</strong> 2023. All Rights Reserved.</p>
+                                        </div>
+                                        <div className="social-accounts">
+                                            <ul>
+                                                <li><Link href="/auth"><i className="fa fa-lock text-danger" /></Link></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="scroll-top">
+                        <div className="scroll-icon">
+                            <i className="fa fa-angle-up" />
+                        </div>
+                    </div>
                 </div>
                 <div className="scroll-top">
                     <div className="scroll-icon">
